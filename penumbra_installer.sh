a0de361d4fcf3e66b0cb877651b5e925b7217ef2
Brendan | WhisperNode <130489602+Brendan-Whispernode@users.noreply.github.com>
2024-03-06 13:34:46 -0400

    Update penumbra_installer.sh
    Signed-off-by: Brendan | WhisperNode <130489602+Brendan-Whispernode@users.noreply.github.com>

--- penumbra_installer.sh
+++ penumbra_installer.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# Author: nodes.bond
-# Penumbra Version: v0.68.
+# Author: WhisperNode
+# Penumbra Version: v0.68.2
 # Go Version: 1.21.1
 # Cometbft Version: v0.37.2
+
+#Stop Current Services
+sudo systemctl stop penumbra
+sudo systemctl stop cometbft
 
 # Check Ubuntu Version
 UBUNTU_VERSION=$(lsb_release -sr)
@@ -12,14 +16,16 @@ if (( $(echo "$UBUNTU_VERSION < 22" | bc -l) )); then
     exit 1
 fi
 
+#Copy Old keys and config file
+echo "Copying validator key and config..."
+cd /home/whispernode/.penumbra/testnet_data/node0/cometbft/config
+sudo cp config.toml /home/whispernode/penumbraconfig.toml
+sudo cp priv_validator_key.json /home/whispernode/penumbra_priv_validator_key.json
+
 # Remove previous versions of Penumbra and related modules
 echo "Removing old versions of Penumbra and related modules..."
-sudo rm -rf /root/penumbra /root/cometbft /root/.local/share/pcli/
-
-# Rename existing Penumbra directory (for updates)
-if [ -d "/root/penumbra" ]; then
-    mv /root/penumbra /root/penumbra_old
-fi
+cd /home/whispernode
+sudo rm -rf /home/whispernode/penumbra /home/whispernode/cometbft /home/whispernode/.local/share/pcli/ /home/whispernode/.penumbra
 
 # Update package list and install dependencies
 sudo apt-get update
@@ -36,10 +42,10 @@ if [ "$CURRENT_GO_VERSION" != "1.21.1" ]; then
 fi
 
 # Set Go environment variables
-echo "export GOROOT=/usr/local/go" >> $HOME/.profile
-echo "export GOPATH=$HOME/go" >> $HOME/.profile
-echo "export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin" >> $HOME/.profile
-source $HOME/.profile
+echo "export GOROOT=/usr/local/go" >> $HOME/.zshrc
+echo "export GOPATH=$HOME/go" >> $HOME/.zshrc
+echo "export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin" >> $HOME/.zshrc
+source $HOME/.zshrc
 
 # Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
@@ -49,14 +55,24 @@ source $HOME/.cargo/env
 git clone https://github.com/penumbra-zone/penumbra
 cd penumbra
 git fetch
-git checkout v0.68.0
+git checkout v0.68.2
 
 # Build pcli and pd
 cargo build --release --bin pcli
 cargo build --release --bin pd
 
+#Move binary files 
+echo "Moving Binaries"
+
+
+# Check Versions
+echo "Checking pd/pcli versions..."
+pd --version
+pcli --version
+
+echo "Installing CometBFT..."
 # Install CometBFT
-cd /root
+cd /home/whispernode
 git clone https://github.com/cometbft/cometbft.git
 cd cometbft
 git checkout v0.37.2
@@ -68,7 +84,7 @@ go mod tidy
 go build -o cometbft ./cmd/cometbft
 
 # Move the compiled executable to the cometbft directory
-mv cometbft /root/cometbft/
+mv cometbft /home/whispernode/cometbft/
 
 # Proceed with installation
 make install
@@ -77,8 +93,8 @@ make install
 ulimit -n 4096
 
 # Request the node name from the user
-echo "Enter the name of your node:"
-read MY_NODE_NAME
+#echo "Enter the name of your node:"
+#read MY_NODE_NAME
 
 # If IP_ADDRESS is empty, prompt the user to enter it manually
 if [ -z "$IP_ADDRESS" ]; then
@@ -94,9 +110,9 @@ if [[ ! $IP_ADDRESS =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
 fi
 
 # Join the testnet with specified external address and moniker
-cd /root/penumbra
+cd /home/whispernode/penumbra
 ./target/release/pd testnet unsafe-reset-all
-./target/release/pd testnet join --external-address $IP_ADDRESS:26656 --moniker "$MY_NODE_NAME"
+./target/release/pd testnet join --external-address $IP_ADDRESS:21956 --moniker $(openssl rand -hex 16)
 
 # Create a new wallet or restore an existing one 
 echo "Do you want to create a new wallet or restore an existing one? [new/restore]"
@@ -116,7 +132,7 @@ else
     exit 1
 fi
 
-PCLI_DIR="/root/.local/share/pcli"
+PCLI_DIR="/home/whispernode/.local/share/pcli"
 
 if [ -d "$PCLI_DIR" ] && [ "$(ls -A $PCLI_DIR)" ]; then
     echo "The pcli directory is not empty. Renaming the existing directory..."
@@ -124,9 +140,12 @@ if [ -d "$PCLI_DIR" ] && [ "$(ls -A $PCLI_DIR)" ]; then
 fi
 
 # Add pcli to the system path for simplified command usage
-echo "export PATH=\$PATH:/root/penumbra/target/release" >> $HOME/.profile
-source $HOME/.profile
+echo "export PATH=\$PATH:/home/whispernode/penumbra/target/release" >> $HOME/.zshrc
+source $HOME/.zshrc
 
-# Launch the node and CometBFT in tmux
-tmux kill-session -t penumbra
-tmux new-session -d -s penumbra '/root/penumbra/target/release/pd start' && tmux split-window -h '/root/cometbft/cometbft start --home ~/.penumbra/testnet_data/node0/cometbft' && tmux attach -t penumbra
+#Swap Validator Key Files 
+echo "Swapping Validator Key"
+cd /home/whispernode/.penumbra/testnet_data/node0/cometbft/config
+cp /home/whispernode/penumbra_priv_validator_key.json priv_validator_key.json 
+
+echo "Change your config and start services!"
