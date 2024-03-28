e38fe8d13494f5f0f8969e490dde314c6e4a6fc4
nodes.bond <150801441+nodesbond@users.noreply.github.com>
2024-03-28 08:24:26 +0600

    Update penumbra_nodes_bond_installer.sh
    v0.70.3

--- penumbra_nodes_bond_installer.sh
+++ penumbra_nodes_bond_installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Author: nodes.bond
-# Penumbra Version: v0.70.1
+# Penumbra Version: v0.70.3
 # Go Version: 1.21.1
 # Cometbft Version: v0.37.5
 
@@ -75,7 +75,7 @@ source $HOME/.cargo/env
 git clone https://github.com/penumbra-zone/penumbra
 cd penumbra
 git fetch
-git checkout v0.70.1
+git checkout v0.70.3
 
 # Build pcli and pd
 cargo build --release --bin pcli
