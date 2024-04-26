40da001443d5ba76808e82c45774241f66025775
nodes.bond <150801441+nodesbond@users.noreply.github.com>
2024-04-26 22:43:16 +0600

    Update penumbra_nodes_bond_installer.sh

--- penumbra_nodes_bond_installer.sh
+++ penumbra_nodes_bond_installer.sh
@@ -24,6 +24,7 @@ if (( $(echo "$UBUNTU_VERSION < 22" | bc -l) )); then
     exit 1
 fi
 
+# Set default GOPATH, GOROOT and update PATH
 export GOPATH=${GOPATH:-"$HOME/go"}
 export GOROOT=${GOROOT:-"/usr/local/go"}
 export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
@@ -32,7 +33,7 @@ export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
 echo "Removing old versions of Penumbra and related modules..."
 sudo rm -rf /root/penumbra /root/cometbft
 
-# Rename existing Penumbra directory (for updates)
+# Rename existing Penumbra directory
 if [ -d "/root/penumbra" ]; then
     mv /root/penumbra /root/penumbra_old
 fi
@@ -68,97 +69,66 @@ sudo apt-get update
 sudo apt-get install -y build-essential pkg-config libssl-dev clang git-lfs tmux libclang-dev curl
 sudo apt-get install tmux
 
-# Check if Go is installed and update it if it is not version 1.21.1
-CURRENT_GO_VERSION=$(go version | grep -oP 'go\K[0-9.]+')
-if [ "$CURRENT_GO_VERSION" != "1.21.1" ]; then
-    echo "Updating Go to version 1.21.1..."
-    sudo rm -rf /usr/local/go
+# Check and install Go if not present
+if ! command -v go > /dev/null; then
+    echo "Go is not installed. Installing Go..."
     wget https://dl.google.com/go/go1.21.1.linux-amd64.tar.gz
     sudo tar -xvf go1.21.1.linux-amd64.tar.gz -C /usr/local
+    export PATH=$PATH:/usr/local/go/bin
 fi
 
 # Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
 
-# Clone Penumbra repository and checkout the specified version
+# Clone and set up Penumbra
 git clone https://github.com/penumbra-zone/penumbra
 cd penumbra
 git fetch
 git checkout v0.73.0
-
-# Build pcli and pd
 cargo build --release --bin pcli
 cargo build --release --bin pd
 
-# Install CometBFT
+# Install and set up CometBFT
 cd /root
 git clone https://github.com/cometbft/cometbft.git
 cd cometbft
 git checkout v0.37.5
-
-# Update Go modules
 go mod tidy
-
-# Compile the cometbft executable
 go build -o cometbft ./cmd/cometbft
+mv cometbft /root/cometbft/
 
-# Move the compiled executable to a specific directory inside /root/cometbft if not already there
-if [ ! -f /root/cometbft/cometbft ]; then
-    mv cometbft /root/cometbft/
-else
-    echo "Executable already in place."
-fi
-
-# Proceed with installation
+# Prepare for node operation
 make install
-
-# Increase the number of allowed open file directors
 ulimit -n 4096
 
-# Request the node name from the user
+# Set up node configuration
 echo "Enter the name of your node:"
 read MY_NODE_NAME
-
-# If IP_ADDRESS is empty, prompt the user to enter it manually
 IP_ADDRESS=$(curl -4s ifconfig.me)
 if [ -z "$IP_ADDRESS" ]; then
-    echo "Could not automatically determine the server's IP address."
-    echo "Please enter the server's external IP address manually:"
+    echo "Could not automatically determine the server's IP address. Please enter it manually:"
     read IP_ADDRESS
 fi
 
-# Validate the IP_ADDRESS input
 if [[ ! $IP_ADDRESS =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     echo "Invalid IP address format. Exiting."
     exit 1
 fi
 
-# Join the testnet with specified external address and moniker
 cd /root/penumbra
-pd testnet unsafe-reset-all
-pd testnet join --external-address $IP_ADDRESS:26656 --moniker "$MY_NODE_NAME"
+./target/release/pd testnet unsafe-reset-all
+./target/release/pd testnet join --external-address $IP_ADDRESS:26656 --moniker "$MY_NODE_NAME"
 
-# Handle non-empty pcli directory more robustly
-PCLI_DIR="/tmp/.local/share/pcli"
-if [ -d "$PCLI_DIR" ]; then
-    if [ "$(ls -A $PCLI_DIR)" ]; then
-        echo "The pcli directory at $PCLI_DIR is not empty."
-        echo "Existing contents will be removed to continue with a clean initialization."
-        rm -rf ${PCLI_DIR:?}/* # Safeguard to prevent accidental deletion of unintended directories
-    fi
-fi
-
-# Proceed with creating or restoring wallet
+# Configure wallet
 echo "Do you want to create a new wallet or restore an existing one? [new/restore]"
 read WALLET_CHOICE
 if [ "$WALLET_CHOICE" = "new" ]; then
-    SEED_PHRASE=$(pcli init soft-kms generate)
+    SEED_PHRASE=$(./target/release/pcli init soft-kms generate)
     echo "Your seed phrase is: $SEED_PHRASE"
     echo "Write down your seed phrase and keep it safe. Press any key to continue."
     read -n 1 -s
 elif [ "$WALLET_CHOICE" = "restore" ]; then
-    pcli init soft-kms import-phrase
     echo "Enter your seed phrase:"
     read SEED_PHRASE
     echo $SEED_PHRASE | ./target/release/pcli init soft-kms import-phrase
@@ -167,14 +137,12 @@ else
     exit 1
 fi
 
-
-# Add pcli to the system path for simplified command usage
 echo "export PATH=\$PATH:/root/penumbra/target/release" >> $HOME/.profile
 source $HOME/.profile
 
-# Launch the node and CometBFT in tmux
+# Manage TMUX sessions
 tmux kill-session -t penumbra
 tmux new-session -d -s penumbra '/root/penumbra/target/release/pd start' && tmux split-window -h '/root/cometbft/cometbft start --home ~/.penumbra/testnet_data/node0/cometbft' && tmux attach -t penumbra
 
-# Restore original home directory at end of script
+# Restore original home directory
 export HOME=$ORIGINAL_HOME
