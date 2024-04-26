9d7309b98a5f824f6b8d28c7c9f2a91da600b054
nodes.bond <150801441+nodesbond@users.noreply.github.com>
2024-04-26 14:49:54 +0600

    Update penumbra_nodes_bond_installer.sh

--- penumbra_nodes_bond_installer.sh
+++ penumbra_nodes_bond_installer.sh
@@ -12,8 +12,6 @@ if (( $(echo "$UBUNTU_VERSION < 22" | bc -l) )); then
     exit 1
 fi
 
-set -euo pipefail
-
 # Remove previous versions of Penumbra and related modules
 echo "Removing old versions of Penumbra and related modules..."
 sudo rm -rf /root/penumbra /root/cometbft
@@ -54,6 +52,8 @@ sudo apt-get update
 sudo apt-get install -y build-essential pkg-config libssl-dev clang git-lfs tmux libclang-dev curl
 sudo apt-get install -y bc  # Ensure bc is installed for version comparison
 sudo apt-get install tmux
+
+set -euo pipefail
 
 # Check if Go is installed and update it if it is not version 1.21.1
 CURRENT_GO_VERSION=$(go version 2>/dev/null | grep -oP 'go\K[0-9.]+') || true
